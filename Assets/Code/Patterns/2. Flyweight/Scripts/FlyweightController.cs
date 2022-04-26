28d3e53ecc163e8135a2ec2d17a51225d598548f
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-04-26 00:16:40 -0300

    Modifies flyweight class for runtime method execution

--- Assets/Code/Patterns/2. Flyweight/Scripts/FlyweightController.cs
+++ Assets/Code/Patterns/2. Flyweight/Scripts/FlyweightController.cs
@@ -9,36 +9,57 @@ namespace Flyweight
     //Switch between Heavy and Flyweight to compare and you should see that the difference is several hundred megabytes even though the data in this case is just 20 doubles
     public class FlyweightController : MonoBehaviour
     {
+        [SerializeField] private bool useFlyWeight;
+        
         private List<Heavy> heavyObjects = new List<Heavy>();
 
         private List<Flyweight> flyweightObjects = new List<Flyweight>();
 
+        private const int numberOfObjects = 1000000;
+
 
         void Start()
         {
-            int numberOfObjects = 1000000;
 
+            if (!useFlyWeight)
+            {
+                InstantiateNormalObjects();
+            }
+            
+            InstantiateFlyweightObjects();
+        }
 
+        [ContextMenu("Instantiate Flyweight Objects")]
+        private void InstantiateFlyweightObjects()
+        {
             //Generate Heavy objects that doesn't share any data
+
+            Data data = new Data();
+
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                Flyweight newFlyweight = new Flyweight(data);
+
+                flyweightObjects.Add(newFlyweight);
+            }
+        }
+
+        [ContextMenu("Instantiate Normal Objects")]
+        private void InstantiateNormalObjects()
+        {
+            //Generate Flyweight objects
+
+            //Generate the data that's being shared among all objects
             for (int i = 0; i < numberOfObjects; i++)
             {
                 Heavy newHeavy = new Heavy();
 
                 heavyObjects.Add(newHeavy);
             }
-
-
-            //Generate Flyweight objects
-
-            //Generate the data that's being shared among all objects
-            //Data data = new Data();
-
-            //for (int i = 0; i < numberOfObjects; i++)
-            //{
-            //    Flyweight newFlyweight = new Flyweight(data);
-
-            //    flyweightObjects.Add(newFlyweight);
-            //}
         }
+
+        
+        
+        
     }
 }
